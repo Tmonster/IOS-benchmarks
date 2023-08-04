a1bd62dc2c7eeb5ec55bb64d59cc60a5cdbbe5e0
Tom Ebergen <tom@ebergen.com>
2023-08-04 18:26:41 +0200

    might have realm working as a db

--- benchmarks/ContentView.swift
+++ benchmarks/ContentView.swift
@@ -18,58 +18,113 @@ struct ContentView: View {
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
         animation: .default)
+    
     private var items: FetchedResults<Item>
     
     private enum ViewState {
-        case loading(Error?)
-        case ready_for_data_conversion(TPCHData)
-        case data_queried(DataFrame)
+        case ready_to_load
+        case loading
+        case data_is_loaded
+        case data_is_queried
+        case converting_data
+        case loading_error
+        case querying_data
+        case data_is_converted
     }
     
-    @State private var state = ViewState.loading(nil)
+    @State private var state = ViewState.ready_to_load
+    @State private var tpch_data : TPCHData?
+    
+//    private init() {
+//        self.tpch_data
+//        
+//    }
     
 
     var body: some View {
-         Group {
-           switch state {
-           case .ready_for_data_conversion(_):
-                Text("Data has been loaded")
-                // execute some simple query in duckdb and get the timing.
-           case .loading(nil):
-               ProgressView { Text("Loading Exoplanets") }
-           case .loading(let error?):
-               Text("Error getting data")
-               Text("\(error.localizedDescription)")
-           case .data_queried(_):
-               Text("works maybe?")
-           default:
-               Text("I don't know")
-           }
-         }
-         .task {
-           await prepareDuckdbTPCH()
-         }
-    }
-
-    private func prepareDuckdbTPCH() async {
-        guard case .loading(_) = state else { return }
-        self.state = .loading(nil)
-        do {
-            let tpch_data = try await TPCHData.create()
-            self.state = .ready_for_data_conversion(tpch_data)
-            self.state = try await .data_queried(tpch_data.GetLineItem())
+        Group {
+            switch state {
+            case .ready_to_load:
+                Text("data is ready to load")
+            case .loading:
+                Text("loading data")
+            case .data_is_loaded:
+                Text("data is loaded")
+            case .data_is_queried:
+                Text("data has been queried")
+            case .querying_data:
+                Text("querying data")
+            case .loading_error:
+                Text("loading error")
+            case .converting_data:
+                Text("converting data to Realm")
+            case .data_is_converted:
+                Text("data is converted")
+            }
         }
-        catch {
-          self.state = .loading(error)
+        VStack {
+            Button("Load Data") {
+                loadData()
+            }
+            .padding()
+            
+            Button("Query Data") {
+                QueryData()
+            }
+            .padding()
+            
+            Button("Convert Data") {
+                ConvertData()
+            }
+            
+            Button("Query Realm Data") {
+                QueryRealmData()
+            }
+            .padding()
         }
     }
     
-    private func ConvertData(tpch_data : TPCHData) {
-        guard case .loading(_) = state else { return }
-        self.state = .loading(nil)
+    private func loadData() {
+        guard case .ready_to_load = state else { return }
+        self.state = .loading
         Task {
-            let lineitem_data = try await tpch_data.GetLineItem();
-            self.state = .data_queried(lineitem_data)
+            do {
+                tpch_data = try await TPCHData.create()
+                self.state = .data_is_loaded
+            }
+            catch {
+                self.state = .loading_error
+            }
+        }
+    }
+    
+    private func QueryRealmData() {
+        Task {
+            try RealmObjects.Query()
+        }
+    }
+    
+    private func ConvertData() {
+        self.state = .converting_data
+        Task {
+            if let tpch_data {
+                let results = try await tpch_data.GetLineItem()
+                try RealmObjects.Load(tpch_data : results)
+            }
+        }
+        self.state = .data_is_converted
+    }
+    
+    private func QueryData() {
+        guard case .data_is_loaded = state else { return }
+        self.state = .querying_data
+        Task {
+            if let tpch_data {
+                let results = try await tpch_data.GetLineItem()
+                self.state = .data_is_queried
+            } else {
+                self.state = .loading_error
+            }
         }
 
     }
--- benchmarks/RealmBenchmarks/RealmObjects.swift
+++ benchmarks/RealmBenchmarks/RealmObjects.swift
@@ -7,6 +7,69 @@
 
 import Foundation
 import RealmSwift
+import DuckDB
+
+final class RealmObjects {
+    static func Load(tpch_data : ResultSet) throws {
+        
+        let configuration = Realm.Configuration(deleteRealmIfMigrationNeeded: true)
+        let realm = try Realm(configuration: configuration)
+        
+        
+        
+        let l_orderkey = tpch_data[0].cast(to: Int.self)
+        let l_partkey = tpch_data[1].cast(to: Int.self)
+        let l_suppkey = tpch_data[2].cast(to: Int.self)
+        let l_linenumber = tpch_data[3].cast(to: Int.self)
+//        let l_quantity = tpch_data[4].cast(to: Decimal.self)
+//        let l_extendedprice = tpch_data[5].cast(to: Decimal.self)
+//        let l_discount = tpch_data[6].cast(to: Decimal.self)
+//        let l_tax = tpch_data[7].cast(to: Decimal.self)
+        let l_returnflag = tpch_data[8].cast(to: String.self)
+        let l_linestatus = tpch_data[9].cast(to: String.self)
+//        let l_shipdate = tpch_data[10].cast(to: Date.self)
+//        let l_commitdate = tpch_data[11].cast(to: Date.self)
+//        let l_receiptdate = tpch_data[12].cast(to: Date.self)
+        let l_shipinstruct = tpch_data[13].cast(to: String.self)
+        let l_shipmode = tpch_data[14].cast(to: String.self)
+        let l_shipcomment = tpch_data[15].cast(to: String.self)
+
+        for i in 0...tpch_data.rowCount {
+            var l_item = Lineitem()
+            l_item.orderKey = l_orderkey[i] ?? l_item.orderKey
+            l_item.partKey = l_partkey[1] ?? l_item.partKey
+            l_item.suppKey = l_suppkey[2] ?? l_item.suppKey
+            l_item.lineNumber = l_linenumber[3] ?? l_item.lineNumber
+//            l_item.quantity = l_quantity[4] ?? l_item.quantity
+//            l_item.extendedPrice = l_extendedprice[5] ?? l_item.extendedPrice
+//            l_item.discount = l_discount[6] ?? l_item.discount
+//            l_item.tax = l_tax[7] ?? l_item.tax
+            l_item.returnFlag = l_returnflag[8] ?? l_item.returnFlag
+            l_item.lineStatus = l_linestatus[9] ?? l_item.lineStatus
+            l_item.shipInstructions = l_shipinstruct[13] ?? l_item.shipInstructions
+            l_item.shipMode = l_shipmode[14] ?? l_item.shipMode
+            l_item.comment = l_shipcomment[15] ?? l_item.comment
+            
+            try! realm.write {
+                realm.add(l_item)
+            }
+        }
+    }
+    
+    static func Query() throws {
+        let configuration = Realm.Configuration(deleteRealmIfMigrationNeeded: true)
+        let realm = try Realm(configuration: configuration)
+        
+        let things = realm.objects(Lineitem.self)
+        
+        let order_keys = things.where {
+            $0.orderKey == 5
+        }
+        
+        print(order_keys)
+    }
+    
+}
 
 class customer: Object {
     @objc dynamic var custKey = 0
@@ -39,15 +102,15 @@ class orders: Object {
     }
 }
 
-class lineitem: Object {
+class Lineitem: Object {
     @objc dynamic var orderKey = 0
     @objc dynamic var partKey = 0
     @objc dynamic var suppKey = 0
     @objc dynamic var lineNumber = 0
-    @objc dynamic var quantity = 0.0
-    @objc dynamic var extendedPrice = 0.0
-    @objc dynamic var discount = 0.0
-    @objc dynamic var tax = 0.0
+    @objc dynamic var quantity : Double = 0.0
+    @objc dynamic var extendedPrice : Double = 0.0
+    @objc dynamic var discount : Double = 0.0
+    @objc dynamic var tax : Double = 0.0
     @objc dynamic var returnFlag = ""
     @objc dynamic var lineStatus = ""
     @objc dynamic var shipDate = Date()
@@ -56,10 +119,6 @@ class lineitem: Object {
     @objc dynamic var shipInstructions = ""
     @objc dynamic var shipMode = ""
     @objc dynamic var comment = ""
-
-    override static func primaryKey() -> String? {
-        return "lineNumber"
-    }
 }
 
 class Nation: Object {
